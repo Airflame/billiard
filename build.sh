db81737d8f0c768b74735258f041a79081535b46
Airflame <airflame@wp.pl>
2018-06-03 19:14:19 +0200

    Minor changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Building server"
-g++ server.cpp src/*.cpp -o server -lsfml-network -lsfml-graphics -lsfml-window -lsfml-system
+g++ server.cpp src/*.cpp -o bin/server -lsfml-network -lsfml-graphics -lsfml-window -lsfml-system
 echo "Building client"
-g++ client.cpp src/*.cpp -o client -lsfml-network -lsfml-graphics -lsfml-window -lsfml-system
+g++ client.cpp src/*.cpp -o bin/client -lsfml-network -lsfml-graphics -lsfml-window -lsfml-system
 echo "Done"
